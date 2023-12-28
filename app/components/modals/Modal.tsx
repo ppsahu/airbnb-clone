22cc49ea1a36c25460a7a7a071b5f31c5c8ec743
ppsahu <pratyushpsahu2@gmail.com>
2023-12-28 14:05:33 -0800

    added content and header into modal

--- app/components/modals/Modal.tsx
+++ app/components/modals/Modal.tsx
@@ -3,6 +3,8 @@
 import React from "react";
 import { useCallback, useEffect, useState } from "react";
 
+import { IoMdClose } from "react-icons/io";
+
 interface ModalProps {
     isOpen?: boolean;
     onClose: () => void;
@@ -80,7 +82,75 @@ const Modal: React.FC<ModalProps> = ({
                 focus:outline-none
                 bg-neutral-800/70
             "
-          ></div>
+          >
+                <div className="
+                    relative 
+                    w-full
+                    md:w-4/6
+                    lg:w-3/6
+                    xl:w-2/5
+                    my-6
+                    mx-auto 
+                    h-full 
+                    lg:h-auto
+                    md:h-auto
+                    "
+                >
+                    {/* CONTENT */}
+                    <div className={`
+                        translate
+                        duration-300
+                        h-full
+                        ${showModal ? 'translate-y-0' : 'translate-y-full'}
+                        ${showModal ? 'opacity-100' : 'opacity-0'}
+                    `}>
+                        <div className="
+                            translate
+                            h-full
+                            lg:h-auto
+                            md:h-auto
+                            border-0 
+                            rounded-lg 
+                            shadow-lg 
+                            relative 
+                            flex 
+                            flex-col 
+                            w-full 
+                            bg-white 
+                            outline-none 
+                            focus:outline-none
+                            ">
+
+                        </div>
+                            {/* HEADER */}
+                            <div className="
+                                flex 
+                                items-center 
+                                p-6
+                                rounded-t
+                                justify-center
+                                relative
+                                border-b-[100px]
+                            ">
+                                <button 
+                                    onClick={handleClose}
+                                    className="
+                                    p-1
+                                    border-0 
+                                    hover:opacity-70
+                                    transition
+                                    absolute
+                                    left-9
+                                ">
+                                    <IoMdClose size={18} />
+                                </button>
+                                <div className="text-lg font-semibold">
+                                    Login Modal
+                                </div>
+                            </div>
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
