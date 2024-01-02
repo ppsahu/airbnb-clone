ff47a6f85e9c66c067afa5c87810e3f85b2d60e5
ppsahu <pratyushpsahu2@gmail.com>
2024-01-02 15:50:24 -0800

    UI for Categories

--- app/components/navbar/Logo.tsx
+++ app/components/navbar/Logo.tsx
@@ -6,6 +6,7 @@ import { useRouter } from "next/navigation";
 const Logo = () => {
   const router = useRouter();
 
+
   return ( 
     <Image
       onClick={() => router.push('/')}
--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -1,5 +1,6 @@
 
 import Container from "../Container";
+import Categories from "./Categories";
 import Logo from "./Logo";
 import Search from "./Search";
 import UserMenu from "./UserMenu";
@@ -35,6 +36,7 @@ const Navbar: React.FC<NavbarProps> = ({
                 </div>
             </Container>
             </div>
+            <Categories />
         </div>
     );
 }
